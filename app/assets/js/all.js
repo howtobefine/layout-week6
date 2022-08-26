0923a5ceadcfef7b54b2cb4f5cbbe355d2453a60
howtobefine <b4808697b@gmail.com>
2022-08-26 16:52:36 +0800

    final version

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,28 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  effect: "fade",
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  controller: {
+    inverse: true,
+  },
+});
+
+const elem = document.querySelector('input[name="datepicker"]');
+const datepicker = new Datepicker(elem, {
+  buttonClass: 'btn',
+  autohide: true,
+  language: "zh-TW",
+  orientation: "bottom"
 });
