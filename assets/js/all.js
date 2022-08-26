275f5256ba50d9817a42badea0496de4db1786d5
howtobefine <b4808697b@gmail.com>
2022-08-26 16:53:04 +0800

    Update 2022-08-26T08:53:02.457Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,28 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  effect: "fade",
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets'
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  controller: {
+    inverse: true
+  }
+});
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
+  buttonClass: 'btn',
+  autohide: true,
+  language: "zh-TW",
+  orientation: "bottom"
 });
 //# sourceMappingURL=all.js.map
